4177c3eaf8ae3da348e58d308dd28ed5d420f71c
harmindersingh <harmindersingh3010@gmail.com>
2021-05-09 22:51:59 +0530

    Project setup

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const route: Routes = [
+  { path: '', component: HomeComponent },
+];
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(route),
+  ]
+})
+export class HomeModule { }
