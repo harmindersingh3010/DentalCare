65af1bf60d06d98100e9a06ef69c6a17b34b0464
Wangkheirakpam Prakash Singh <wangkheirakpam.singh@agreeya.net>
2021-05-10 18:02:23 +0530

    carousal added in testimonial and blogs

--- src/app/components/home-section-container/blog/blog.component.ts
+++ src/app/components/home-section-container/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { DoctorsModel } from '../doctors/doctors.component';
 
 @Component({
   selector: 'app-blog',
@@ -7,6 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+  doctors: any = [{
+    name: 'Dr. Linda Feldman',
+    img: 'assets/images/team/1.jpg',
+    specialization: 'Root Canals Dentist',
+    _id: '1'
+  }, {
+    name: 'Dr. Jessica Tailor',
+    img: 'assets/images/team/2.jpg',
+    specialization: 'Implant Surgeon',
+    _id: '2'
+  }, {
+    name: 'Dr. Nicholas Fleming',
+    img: 'assets/images/team/3.jpg',
+    specialization: 'Cosmetic Dental Surgeon',
+    _id: '3'
+  }, {
+    name: 'Dr. Brian Adam',
+    img: 'assets/images/team/4.jpg',
+    specialization: 'Restorative Dentist',
+    _id: '4'
+  }];
+
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    margin: 15,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 2
+      },
+      940: {
+        items: 3
+      }
+    },
+    nav: false
+  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
